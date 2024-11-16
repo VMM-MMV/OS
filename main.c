129cb34203a0c833e6804f68515603c6266c251a
vmm-mmv <mihai.vieru@isa.utm.md>
2024-11-16 16:12:30 +0200

    added better random times inbetween calculations

--- main.c
+++ main.c
@@ -9,45 +9,56 @@
 
 sem_t producer_sem;
 sem_t consumer_sem;
-// Pipe file descriptors
 int pipe_fd[2];
 
+// Function to get current semaphore value
 int get_sem_value(sem_t *sem) {
-    // Function to get current semaphore value
     int val;
     sem_getvalue(sem, &val);
     return val;
 }
 
+void random_delay(int min_ms, int max_ms) {
+    int delay = min_ms + rand() % (max_ms - min_ms + 1);
+    usleep(delay * 1000); // Convert to microseconds
+}
+
 void* writer_thread(void* arg) {
     while(1) {
+        int sem_id = get_sem_value(&producer_sem);
         sem_wait(&producer_sem);
+        
+        random_delay(100, 500);  // 100-500ms delay
         
         int input_number = rand() % 101;
         write(pipe_fd[1], &input_number, sizeof(input_number));
         
         printf("Writer thread [Producer sem ID: P%d]: Number sent: %d\n", 
-               get_sem_value(&producer_sem), input_number);
+               sem_id, input_number);
+               
+        // Add random delay before consumer post to allow semaphore value to vary
+        random_delay(50, 200);
     }
     return NULL;
 }
 
 void* reader_thread(void* arg) {
     while(1) {
+        int sem_id = get_sem_value(&consumer_sem);
         sem_wait(&consumer_sem);
+        
+        random_delay(150, 400);  // 150-400ms delay
         
         int output_number;
         read(pipe_fd[0], &output_number, sizeof(output_number));
         
         printf("Reader thread [Consumer sem ID: C%d]: Number received: %d\n", 
-               get_sem_value(&consumer_sem), output_number);
+               sem_id, output_number);
         
-        // Post to both semaphores and show their new values
         sem_post(&producer_sem);
-        printf("Released producer_sem (new ID: P%d)\n", get_sem_value(&producer_sem));
-        
+        random_delay(50, 150);
         sem_post(&consumer_sem);
-        printf("Released consumer_sem (new ID: C%d)\n", get_sem_value(&consumer_sem));
+        random_delay(50, 200);
     } 
     return NULL;
 }
@@ -55,35 +66,33 @@ void* reader_thread(void* arg) {
 int main() {
     srand(time(NULL));
     
-    // Create an unnamed pipe
     if (pipe(pipe_fd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
-
+    
     sem_init(&producer_sem, 0, 3);
     sem_init(&consumer_sem, 0, 5);
-    printf("Initial producer_sem value (ID: P%d)\n", get_sem_value(&producer_sem));
-    printf("Initial consumer_sem value (ID: C%d)\n", get_sem_value(&consumer_sem));
     
-    // Create writer and reader threads
     pthread_t writer, reader;
     
     if (pthread_create(&writer, NULL, writer_thread, NULL) != 0) {
         perror("Failed to create writer thread");
         exit(EXIT_FAILURE);
     }
+    
     if (pthread_create(&reader, NULL, reader_thread, NULL) != 0) {
         perror("Failed to create reader thread");
         exit(EXIT_FAILURE);
     }
-
+    
     pthread_join(writer, NULL);
     pthread_join(reader, NULL);
-
+    
     close(pipe_fd[0]);
     close(pipe_fd[1]);
     sem_destroy(&producer_sem);
     sem_destroy(&consumer_sem);
+    
     return 0;
 }
